c4d916919d86fb8cc3a8697929ef184fbfe6d9f1
Tursunov Azizbek <agtursunov@gmail.com>
2025-02-18 22:54:36 +0500

    Task_1 solution is changed

--- lesson-3/homework/Task_1.sql
+++ lesson-3/homework/Task_1.sql
@@ -3,29 +3,35 @@ GO
 
 SELECT * FROM Employees;
 GO
-
+/*Selects the top 10% highest-paid employees.*/
 SELECT TOP 10 PERCENT
 *
 FROM Employees
 ORDER BY Salary DESC;
 GO
-
+/*Groups them by department and calculates the average salary per department.*/
 SELECT Department, 
        AVG(Salary) AS AverageSalary
 FROM Employees
 GROUP BY Department;
-
-SELECT *, 
-	'High' AS SalaryCategory
-FROM Employees
-Where Salary > 80000
-	UNION ALL
+GO
+/*Displays a new column SalaryCategory:
+'High' if Salary > 80,000
+'Medium' if Salary is between 50,000 and 80,000
+'Low' otherwise.*/
 SELECT *,
-	'Medium' AS SalaryCategory
+	CASE
+		WHEN Salary > 80000 THEN 'High'
+		WHEN Salary BETWEEN 50000 AND 80000 THEN 'Medium'
+		WHEN Salary < 50000 THEN 'Low'
+	END AS SalaryCategory
+FROM Employees;
+GO
+/*Orders the result by Salary descending.
+Skips the first 2 records and fetches the next 5.*/
+SELECT *
 FROM Employees
-WHERE 50000 <= Salary AND Salary <= 80000
-	UNION ALL
-SELECT *,
-	'Low' AS SalaryCategory
-FROM Employees
-WHERE Salary < 50000
+ORDER BY Salary DESC
+OFFSET 2 ROWS
+FETCH NEXT 5 ROWS ONLY;
+GO
