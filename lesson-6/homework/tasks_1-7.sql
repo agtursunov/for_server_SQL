e39b82639a4df06e80aca3ba81a8abc72ce948ee
Tursunov Azizbek <agtursunov@gmail.com>
2025-03-02 18:23:36 +0500

    Task_6 is changed

--- lesson-6/homework/tasks_1-7.sql
+++ lesson-6/homework/tasks_1-7.sql
@@ -3,47 +3,47 @@ GO
 
 -- 1. Write a query to get a list of employees along with their department names.
 
-SELECT EmployeeID,
-	[Name],
-	Salary,
-	DepartmentName
+SELECT e.EmployeeID,
+	e.[Name],
+	e.Salary,
+	d.DepartmentName
 FROM Employees AS e
 INNER JOIN Departments AS d
 	ON e.DepartmentID = d.DepartmentID
 
 -- 2. Write a query to list all employees, including those who are not assigned to any department.
 
-SELECT  EmployeeID,
-	[Name],
-	Salary,
-	DepartmentName
+SELECT e.EmployeeID,
+	e.[Name],
+	e.Salary,
+	d.DepartmentName
 FROM Employees AS e
 LEFT OUTER JOIN Departments AS d
 	ON e.DepartmentID = d.DepartmentID
 
 -- 3. Write a query to list all departments, including those without employees.
 
-SELECT EmployeeID,
-	[Name],
-	Salary,
-	DepartmentName
+SELECT e.EmployeeID,
+	e.[Name],
+	e.Salary,
+	d.DepartmentName
 FROM Employees AS e
 RIGHT OUTER JOIN Departments AS d
 	ON e.DepartmentID = d.DepartmentID
 
 -- 4. Write a query to retrieve all employees and all departments, even if there’s no match between them.
 
-SELECT EmployeeID,
-	[Name],
-	Salary,
-	DepartmentName
+SELECT e.EmployeeID,
+	e.[Name],
+	e.Salary,
+	d.DepartmentName
 FROM Employees AS e
 FULL OUTER JOIN Departments AS d
 	ON e.DepartmentID = d.DepartmentID
 
 -- 5. Write a query to find the total salary expense for each department.
 
-SELECT DepartmentName,
+SELECT d.DepartmentName,
 	SUM(Salary)
 FROM Employees AS e
 INNER JOIN Departments AS d
@@ -52,11 +52,11 @@ GROUP BY DepartmentName
 
 -- 6. Write a query to generate all possible combinations of departments and projects.
 
-SELECT [Name],
-	Salary,
-	DepartmentName
-FROM Employees, Departments
-
+SELECT
+	p.ProjectName,
+	d.DepartmentName
+FROM Departments AS d
+CROSS JOIN Projects AS p
 /* 7. Write a query to get a list of employees with their
 department names and assigned project names. Include employees even if they don’t have a project.*/
 SELECT
